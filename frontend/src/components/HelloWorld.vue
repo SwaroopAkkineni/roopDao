287c86f436ac582e2c3086aa8798f67f9dd8610c
Swaroop Akkineni <swa7@pitt.edu>
2022-05-07 18:10:44 -0400

    004 simple nav text

---
+++ frontend/src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="navbar">
+      <h1 class="item">Home</h1>
+      <h1 class="item">Account</h1>
+      <h1 class="item">Buy</h1>
+      <h1 class="item">Sell</h1>
+    </div>
+    <div class="body">
+      <h1>{{ msg }}</h1>
+      <p>Here is the home page</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    msg: String,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.body {
+  background-color: lightblue;
+}
+
+.navbar {
+  background-color: lightgreen;
+  text-align: left;
+
+  h1 {
+    color: red;
+    display: inline;
+    margin: 0 10px 0 10px;
+  }
+}
+
+h3 {
+  margin: 40px 0 0;
+}
+
+a {
+  color: #42b983;
+}
+</style>
