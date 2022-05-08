16dbff4f73a5a503bed55adb0b79a3d9901c859a
Swaroop Akkineni <swa7@pitt.edu>
2022-05-07 21:58:55 -0400

    004 nav bar

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,48 +1,70 @@
 <template>
   <div>
     <div class="navbar">
-      <h1 class="item">Home</h1>
-      <h1 class="item">Account</h1>
-      <h1 class="item">Buy</h1>
-      <h1 class="item">Sell</h1>
+      <button class="button" @click="render('Home')">Home</button>
+      <button class="button" @click="render('Account')">Account</button>
+      <button class="button" @click="render('Trade')">Trade</button>
     </div>
-    <div class="body">
-      <h1>{{ msg }}</h1>
-      <p>Here is the home page</p>
-    </div>
+    <div>{{ message }} page</div>
   </div>
 </template>
 
 <script>
+// import BodyA from './BodyA.vue'
+// import NavBarButton from './navbar/NavBarButton.vue'
+
 export default {
   props: {
     msg: String,
+  },
+  components: {
+    // BodyA,
+    // NavBarButton,
+  },
+  methods: {
+    render(msg) {
+      this.message = msg
+    },
+  },
+  data() {
+    return {
+      message: 'Home',
+    }
   },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.body {
-  background-color: lightblue;
-}
-
 .navbar {
-  background-color: lightgreen;
-  text-align: left;
+  display: inline-flex;
 
-  h1 {
-    color: red;
-    display: inline;
-    margin: 0 10px 0 10px;
+  .button {
+    display: inline-block;
+    margin: 0.5em 0;
+    padding: 1em 2em;
+    background: #fff;
+    border: 2px solid tomato;
+    border-radius: 3px;
+    color: tomato;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1em;
+    font-weight: 700;
+    letter-spacing: 0.02em;
+    line-height: 1;
+    text-decoration: none;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+
+  .button:hover {
+    background: tomato;
+    color: #fff;
   }
 }
 
 h3 {
   margin: 40px 0 0;
-}
-
-a {
-  color: #42b983;
 }
 </style>
