fac4fc64657ca5ceaf351789a3a0e88d8f9b9a06
Swaroop Akkineni <swa7@pitt.edu>
2022-05-15 15:20:48 -0400

    005 some stuff

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,31 +1,23 @@
 <template>
-  <div>
-    <div class="navbar">
-      <button class="button" @click="render('Home')">Home</button>
-      <button class="button" @click="render('Account')">Account</button>
-      <button class="button" @click="render('Trade')">Trade</button>
-    </div>
+  <div class="helloWorld">
+    <NavBar />
     <div>{{ message }} page</div>
   </div>
 </template>
 
 <script>
-// import BodyA from './BodyA.vue'
-// import NavBarButton from './navbar/NavBarButton.vue'
+// import AccountComponent from './AccountComponent.vue'
+import NavBar from './navBar/NavBar.vue'
 
 export default {
   props: {
     msg: String,
   },
   components: {
-    // BodyA,
-    // NavBarButton,
+    // AccountComponent,
+    NavBar,
   },
-  methods: {
-    render(msg) {
-      this.message = msg
-    },
-  },
+
   data() {
     return {
       message: 'Home',
@@ -36,35 +28,11 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-.navbar {
-  display: inline-flex;
+.helloWorld {
+  text-align: center;
 
-  .button {
-    display: inline-block;
-    margin: 0.5em 0;
-    padding: 1em 2em;
-    background: #fff;
-    border: 2px solid tomato;
-    border-radius: 3px;
-    color: tomato;
-    font-family: 'Quicksand', sans-serif;
-    font-size: 1em;
-    font-weight: 700;
-    letter-spacing: 0.02em;
-    line-height: 1;
-    text-decoration: none;
-    text-transform: uppercase;
-    cursor: pointer;
-    transition: 0.3s;
+  h3 {
+    margin: 40px 0 0;
   }
-
-  .button:hover {
-    background: tomato;
-    color: #fff;
-  }
-}
-
-h3 {
-  margin: 40px 0 0;
 }
 </style>
---
+++ frontend/src/components/navBar/NavBar.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="navbar">
+    <button class="button" @click="render('Home')">Home</button>
+    <button class="button" @click="render('Account')">Account</button>
+    <button class="button" @click="render('Trade')">Trade</button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    render(msg) {
+      this.$emit('message', msg)
+    },
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.navbar {
+  display: inline-flex;
+
+  .button {
+    display: inline-block;
+    margin: 0.5em 0;
+    padding: 1em 2em;
+    background: #fff;
+    border: 2px solid tomato;
+    border-radius: 3px;
+    color: tomato;
+    font-family: 'Quicksand', sans-serif;
+    font-size: 1em;
+    font-weight: 700;
+    letter-spacing: 0.02em;
+    line-height: 1;
+    text-decoration: none;
+    text-transform: uppercase;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+
+  .button:hover {
+    background: tomato;
+    color: #fff;
+  }
+}
+</style>
